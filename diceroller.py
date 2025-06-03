4bf40130039e871cc0a8b19a7df99a900f7450df
DrinkLemonade <117670511+DrinkLemonade@users.noreply.github.com>
2025-06-03 16:08:34 +0200

    Armor, DR
    -Added armor, DR, armor bonus to Soak (except Soak rerolls)
    -Shove weapons, armor and everything into a general content DB actually, why not :)
    -damage_mitigation renamed to soak_roll
    -Pawn templates now defined before simulation, copies are created via deep copying at simulation start
    -HP loss now occurs outside attack()

--- diceroller.py
+++ diceroller.py
@@ -1,11 +1,13 @@
 import random
 import logging
+import copy
 
 class Pawn:
     def __init__(self, name, attack, hp, shell):
         self.name = name
         self.attack = attack
         self.weapon = Weapon()
+        self.armor = None
         self.hp = hp
         self.shell = shell
 
@@ -23,28 +25,34 @@ class Weapon(Item):
         self.name = name
         self.damage = damage
 
+class Armor(Item):
+    def __init__(self, id = "ARMOR_ID", name = "Default Armor", dr = 0, soak = 0, soak_reroll = 0):
+        super().__init__(name)
+        self.id = id
+        self.name = name
+        self.dr = dr
+        self.soak = soak
+        self.soak_reroll = soak_reroll
+
+# We're taking advantage of Python's type-flexible (?) arrays and shoving everything into the same DB.
 class DB:
     def __init__(self):
         self.contents = []
+        self.contents.append(Weapon("WEAPON_DEBUG_NO_EFFECT", "Debug Weapon", damage = 2))
+        self.contents.append(Weapon("WEAPON_NAIL", "Nail", damage = 2))
     
     def find(self, id, check_type = None):
         for item in self.contents:
             if (check_type == None or isinstance(item, check_type)) and item.id == id:
                 return item
         return None
-
-class ItemDB(DB):
-    def __init__(self):
-        super().__init__()
-        self.contents.append(Weapon("WEAPON_DEBUG_NO_EFFECT", "Debug Weapon", damage = 2))
-        self.contents.append(Weapon("WEAPON_NAIL", "Nail", damage = 2))
         
 def roll_attack(attack_dice, target_number=5, deadeye=False):
     """Roll attack dice and count successes."""
     successes = roll_dice(attack_dice, target_number, count_sixes = deadeye)
     return successes
 
-def damage_mitigation(soak_dice, damage):
+def soak_roll(soak_dice, damage):
     """Roll Soak dice and reduce damage."""
     successes = roll_dice(num_dice = soak_dice)
     final_damage = max(0, damage - successes)
@@ -71,34 +79,39 @@ def roll_dice(num_dice, target_number=5, count_sixes=False, reroll_count=0):
             successes += 1
         rolls[i] = new_roll  # Replace the lowest roll with the new roll
 
+    logger.debug(f"{rolls} - {successes} successes")
     return successes
 
 
-def simulate_battle(num_battles, attack_dice, override_p1_damage = 0, use_deadeye=False, use_guard_breaker=False):
+def simulate_battle(num_battles, pawn_template_1, pawn_template_2, override_p1_attack = 0, override_p1_damage = 0, use_deadeye=False, use_guard_breaker=False):
     """Simulate a battle between two pawns and track average damage per turn."""
     total_turns = 0
     total_damage = 0
-    db = ItemDB()
 
     for _ in range(num_battles):
-        pawn1 = Pawn("Beetle", attack = attack_dice, hp = 10, shell = 3)
-        pawn1.weapon = db.find("WEAPON_NAIL")
+        # A deep copy's objects are also copies of the original's objects, rather than references to them.
+        pawn1 = copy.deepcopy(pawn_template_1)
+        pawn2 = copy.deepcopy(pawn_template_2)
+
+        if override_p1_attack > 0:
+            pawn1.attack = override_p1_attack
         if override_p1_damage > 0:
-            pawn1.weapon.damage = override_p1_damage
-        pawn2 = Pawn("Ant", attack = 1, hp = 8, shell = 3)
-        pawn2.weapon = db.find("WEAPON_NAIL")
+            pawn1.weapon.damage = override_p1_attack
   
         guard_breaker_modifier = 0
 
         while pawn1.hp > 0 and pawn2.hp > 0:
             # Pawn 1 attacks Pawn 2
             damage_taken = attack(pawn1, pawn2, 5 + guard_breaker_modifier, use_deadeye)
+            pawn2.hp -= damage_taken
+
             total_damage += damage_taken
             if damage_taken > 0 and use_guard_breaker:
                 guard_breaker_modifier = -1
 
             # Pawn 2 attacks Pawn 1
             damage_taken = attack(pawn2, pawn1, 5)
+            pawn1.hp -= damage_taken
 
             total_turns += 1
         if pawn1.hp <= 0:
@@ -113,10 +126,29 @@ def attack(attacker, defender, base_difficulty, use_deadeye=False):
     successes = roll_attack(attacker.attack, base_difficulty, use_deadeye)
     if successes > 0:
         damage_risk = min(attacker.weapon.damage + successes - 1, 2 * attacker.weapon.damage)
-        damage_taken = damage_mitigation(defender.shell, damage_risk)
-        defender.hp -= damage_taken
-        logger.debug(f"{attacker.name} attacks {defender.name} and deals {damage_taken} damage.")
-        return damage_taken
+        logger.debug(f"{attacker.name} attacks {defender.name} with {attacker.weapon.name}! {successes} successes, {damage_risk} damage risk.")
+
+        armor_soak = 0
+        # TODO add armor rerolls
+        dr_text = ""
+        soak_text = ""
+        
+        if defender.armor != None:
+            dr = defender.armor.dr
+            armor_soak = defender.armor.soak
+            # Apply DR, which can't reduce risk below 1
+            pre_dr_risk = damage_risk
+            damage_risk = max(damage_risk - dr, 1)
+            if damage_risk < pre_dr_risk:
+                dr_text = f"(damage: {pre_dr_risk} to {damage_risk} after DR)"
+
+        pre_soak_risk = damage_risk
+        damage_risk = soak_roll(defender.shell + armor_soak, damage_risk)
+        if damage_risk < pre_soak_risk:
+            soak_text = f"(damage: from {pre_soak_risk} to {damage_risk} after Soak)"
+            
+        logger.debug(f"{damage_risk} damage! New health is {defender.hp}. {dr_text} {soak_text}")
+        return damage_risk
     else:
         logger.debug(f"{attacker.name}'s attack misses {defender.name}.")
         return 0
@@ -127,11 +159,9 @@ def attack(attacker, defender, base_difficulty, use_deadeye=False):
 
 # create logger
 logger = logging.getLogger('DiceLogger')
-logger.setLevel(logging.INFO)
 
 # create console handler and set level to debug
 ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
 
 # create formatter
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -142,14 +172,33 @@ ch.setFormatter(formatter)
 # add ch to logger
 logger.addHandler(ch)
 
-# Example usage
+#Prepare Simulation
 result_list = []
-num_battles = 300
+
+#Simulation Settings
+num_battles = 1
+debug_level = logging.DEBUG # logging.INFO
+
+logger.setLevel(debug_level)
+ch.setLevel(debug_level)
+
+# P1's attack dice count doesn't matter since we're overriding it to test ranges from 1 to 7.
+content_db = DB()
+
+pawn1 = Pawn("Beetle", attack = 3, hp = 10, shell = 3)
+pawn1.weapon = content_db.find("WEAPON_NAIL")
+
+pawn2 = Pawn("Ant", attack = 1, hp = 8, shell = 3)
+pawn2.weapon = content_db.find("WEAPON_NAIL")
+
+override_p1_attack = 0
+override_p1_damage = 0
+
 
 # Reminder: range's last value is not inclusive
 for simulate_damage in range(1, 5):
     for simulate_attack in range (1, 8):
-        average_damage = simulate_battle(num_battles, simulate_attack, simulate_damage)
+        average_damage = simulate_battle(num_battles, pawn1, pawn2, override_p1_attack, override_p1_damage)
         result_text = f"Average damage per attack for {simulate_attack} attack dice and {simulate_damage} damage: {average_damage:.2f}"
         result_list.append(result_text)
 
